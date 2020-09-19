152fff9eeebe0bbab2a69c57bada14402d9302d0
Jerson Suaza <jerson.suaza@gmail.com>
2020-09-19 11:35:27 -0500

    form booking data done

--- src/pages/Booking/index.js
+++ src/pages/Booking/index.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Page } from '../Page'
 import { Button } from '../../components/Button'
+import { useParams } from 'react-router-dom'
 
 export const Booking = () => {
+
+  const { id } = useParams()
+  const [bookingDate, setBookingDate] = useState('')
+  const [comments, setComments] = useState('')
+  const [isValidForm, setIsValidForm] = useState(false)
+
+  useEffect(() => {
+    setIsValidForm(new Date(bookingDate) > new Date())
+  }, [bookingDate])
+
+  const bookingFormHandler = (e) => {
+    e.preventDefault()
+    const form = {
+      idExperience: id,
+      bookingDate,
+      comments // comments: comments
+    }
+    console.log('formulario a enviar', form)
+  }
+
   return (
     <Page>
       <h3>Formulario de reserva</h3>
-      <form className="form">
+      <form onSubmit={bookingFormHandler} className="form">
         <div>
           <label>Fecha de la reserva</label>
-          <input type="date" />
+          <input value={bookingDate} type="date"
+            onChange={ e => setBookingDate(e.target.value) }
+          />
         </div>
+        { bookingDate }
         <div>
           <label>Comentarios adicionales</label>
-          <textarea></textarea>
+          <textarea value={comments} onChange={ e => setComments(e.target.value) } ></textarea>
         </div>
-        <Button disabled={true} label="Enviar reserva" type="submit" />
+        <Button disabled={!isValidForm} label="Enviar reserva" type="submit" />
       </form>
     </Page>
   )
